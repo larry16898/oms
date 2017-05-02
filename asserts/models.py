a457d9c6cf886b038db90ec9585c2bee63df1e01
larry <larry@tb-gaming.com>
2017-05-02 20:18:58 +0800

    split settings module

--- asserts/models.py
+++ asserts/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Asserts(models.Model):
+    name = models.CharField(max_length=50,verbose_name='资产名称')
+    
---
+++ project/settings/pro.py
@@ -0,0 +1,16 @@
+from .base import *
+
+DEBUG = False
+
+ADMINS = (
+    ('Larry','larry16898@gmail.com'),
+)
+
+ALLOWED_HOSTS = ['www.test.com',]
+
+DATABASES = {
+    'default': {
+        
+    }
+}
+
