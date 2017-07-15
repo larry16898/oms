ad6908474393e3366c74fa1acb223c1a892bb7f5
larry <larry@tb-gaming.com>
2017-07-15 17:18:05 +0800

    change the order of API url in url root conf.

--- project/urls.py
+++ project/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('accounts.urls', namespace='accounts')),
     url(r'^api/clientdeploy/', include('clientdeploy.urls')),
+    url(r'^', include('accounts.urls', namespace='accounts')),
+
 ]
